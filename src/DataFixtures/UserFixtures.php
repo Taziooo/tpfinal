36f85dd405bec909ce3ab59e970d952e3394f5c1
Taziooo <tazio.giampiccolo@gmail.com>
2025-12-19 10:31:25 +0100

    Ajoute des fixtures pour avoir un user de test

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('admin@test.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, 'admin1234')
+        );
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
